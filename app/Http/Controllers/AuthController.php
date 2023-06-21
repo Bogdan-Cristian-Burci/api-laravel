3a7aa81e9a3bc16531c07f740513372379deb9a3
Bogdan-Cristian-Burci <bogdan.cristian.burci@gmail.com>
2023-06-21 17:02:38 +0300

    create landing page for email validation(html, css, images),add new fields to user model: first_name, last_name, phone_number, change seeders, database migrations, controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\ForgotPasswordRequest;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Http\Requests\ValidateTokenRequest;
+use App\Models\PasswordReset;
+use App\Models\Training;
+use App\Models\User;
+use App\Models\UserTraining;
+use App\Notifications\PasswordResetNotification;
+use App\Transformers\User\CreateUserTransformer;
+use App\Transformers\User\LoginTransformer;
+use Exception;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Support\Facades\Hash;
+use Log;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+
+class AuthController extends ApiController
+{
+
+    public function register(CreateUserRequest $request)
+    {
+        $user = User::create([
+            'first_name'=>$request->input('first_name'),
+            'last_name'=>$request->input('last_name'),
+            'email'=>$request->input('email'),
+            'phone'=>$request->input('phone'),
+            'password'=>Hash::make($request->input('password'))
+        ]);
+
+        $availableTrainings = Training::all();
+
+        event(new Registered($user));
+
+        foreach ($availableTrainings as $training){
+
+            UserTraining::create([
+                'training_id'=>$training->id,
+                'user_id'=>$user->id,
+                'active'=>false,
+                'expire_at'=>null
+            ]);
+        }
+
+        $data = fractal($user, new CreateUserTransformer())
+            ->toArray();
+
+        return $this->successResponse($data,'User created successfully',201);
+    }
+
+    public function login(LoginUserRequest $request){
+
+        try{
+            $user = User::where('email',$request->input('email'))->first();
+
+            if(Hash::check($request->input('password'), $user->password)){
+
+                $token = $user->createToken($request->input('device'))->plainTextToken;
+
+                $data = fractal($user, new LoginTransformer())->addMeta([
+                    'token'=>$token
+                ])->toArray();
+
+                return $this->successResponse($data);
+            }
+        }catch(Exception $e){
+            Log::error('Error on login: ' . $e->getMessage());
+            return $this->errorResponse($e->getCode(), $e->getMessage());
+        }
+        return $this->errorResponse(500,'Something went wrong, try again later');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function forgotPassword(ForgotPasswordRequest $request){
+
+        $user = User::where('email',$request->input('email'))->first();
+
+        //Generate 4 digit random token
+        $resetPasswordToken = str_pad(random_int(1,9999),4,'0', STR_PAD_LEFT);
+
+        $userPassResetRecord = PasswordReset::where('email', $user->email)->first();
+
+        if(!$userPassResetRecord){
+            PasswordReset::create([
+                'email'=>$user->email,
+                'token'=>$resetPasswordToken
+            ]);
+        }else{
+            $userPassResetRecord->update([
+                'email'=>$user->email,
+                'token'=>$resetPasswordToken
+            ]);
+        }
+
+        $user->notify(
+            new PasswordResetNotification(
+                $resetPasswordToken
+            )
+        );
+
+        return $this->successResponse(null,'A code has been sent to your email address.');
+
+    }
+
+
+    public function validateToken(ValidateTokenRequest $request){
+
+        $user = User::where('email',$request->input('email'))->first();
+
+        $userPassResetRecord = PasswordReset::where('email', $user->email)->first();
+
+        if($userPassResetRecord->token == $request->input('token')){
+            $userPassResetRecord->validated = true;
+            $userPassResetRecord->save();
+
+            return $this->successResponse(null,'Token validated with success');
+        }
+
+        return $this->errorResponse(400,'Token does dot match');
+    }
+
+    /**
+     * @throws TokenMismatchException
+     */
+    public function resetPassword(ResetPasswordRequest $request){
+
+        $user = User::where('email', $request->input('email'))->first();
+
+        if(!$user) throw new NotFoundResourceException('user with this email was not found');
+
+        $resetRequest = PasswordReset::where('email', $user->email)->first();
+
+        if(!$resetRequest->validated) throw new TokenMismatchException('Unauthorized request' ,401);
+
+        //Update user's password
+        $user->fill([
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        $user->save();
+
+        //Delete all previous tokens
+        $user->tokens()->delete();
+
+        $resetRequest->delete();
+
+        $token = $user->createToken($request->input('device'))->plainTextToken;
+
+        $data = fractal($user, new LoginTransformer())->addMeta([
+            'token'=>$token
+        ])->toArray();
+
+        return $this->successResponse($data);
+    }
+
+    public function logout(){
+
+        auth()->user()->tokens()->delete();
+
+        return $this->successResponse(null,'User logout successfully');
+    }
+    public function verifyEmail(Request $request){
+
+        $user = User::find($request->route('id'));
+
+        if($user->hasVerifiedEmail()){
+            return redirect('/api/email/verify/already-success');
+        }
+
+        if($user->markEmailAsVerified()){
+            event( new Verified($user));
+        }
+
+        return redirect('/api/email/verify/success');
+    }
+
+    public function emailSuccess(){
+        return view('mail.verify.success');
+    }
+
+    public function alreadyChecked(){
+        return view('mail.verify.already-success');
+    }
+}
