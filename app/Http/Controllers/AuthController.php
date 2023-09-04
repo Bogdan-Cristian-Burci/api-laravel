5da235a8581648aff7a989634ca8ce8e87f68177
Bogdan-Cristian-Burci <bogdan.cristian.burci@gmail.com>
2023-09-04 13:25:49 +0300

    login route check if email is verified?

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -78,7 +78,6 @@ class AuthController extends ApiController
             $user = User::where('email',$request->input('email'))->first();
 
             if(!$user) throw new NotFoundResourceException('User not found');
-            if(!user->hasVerifiedEmail()) throw new AuthenticationException('Verifica adresa de email');
 
             if(Hash::check($request->input('password'), $user->password)){
 
--- routes/api.php
+++ routes/api.php
@@ -71,7 +71,7 @@ Route::middleware(['auth:sanctum','verified'])->group(function () {
 });
 Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 Route::post('register',[AuthController::class,'register']);
-Route::post('login',[AuthController::class,'login']);
+Route::post('login',[AuthController::class,'login'])->middleware('verified');
 Route::post('forgot-password', [AuthController::class,'forgotPassword']);
 Route::post('reset-password', [AuthController::class,'resetPassword']);
 Route::post('validate-token', [AuthController::class,'validateToken']);
